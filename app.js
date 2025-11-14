61a29be4a26bd579b5759e386f5382564ef1c966
Vishal Vashishth <vishal112vp@gmail.com>
2025-11-14 10:53:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// let n= 5;
+
+// for(i=0; i < n; i++) {
+//     console.log("Iteration number: " + i);
+// };
+
+// console.log(Process);
+
+
+const math = require('./math');             // importing the object from the math.js file
+console.log(math);                        // logging the imported object
+
+// console.log("Sum: " + math.sum(5, 3));          // using the functions from the imported object
+// console.log("Multiply: " + math.multiply(5, 3));
+// console.log("Subtract: " + math.subtract(5, 3));
---
+++ math.js
@@ -0,0 +1,27 @@
+
+
+module.exports = 222;        // exporting a number
+
+//1st way to export the functions as an object
+
+
+// const sum = (a, b) => a + b;
+// const multiply = (a, b) => a * b;
+// const subtract = (a, b) => a - b;
+
+// let obj = {                         // creating an object to hold the functions
+//     sum: sum,               
+//     multiply: multiply,
+//     subtract: subtract
+// };
+
+// module.exports = obj;               // exporting the object to the second file
+
+
+
+//2nd way to export the functions as an object
+
+// module.exports.sum = (a, b) => a + b;
+// module.exports.multiply = (a, b) => a * b;
+// exports.subtract = (a, b) => a - b;          // we can export using exports keyword as well
+
