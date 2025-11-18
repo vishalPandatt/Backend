7c4f3f6ab3e2579f116d5eb593b109b101132ba6
Vishal Vashishth <vishal112vp@gmail.com>
2025-11-18 12:03:24 +0530

    learning routing

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -4,8 +4,38 @@ const app = express();
 // console.dir(app);   // to see all properties of express app
 
 
-let port = 3000;            // exchange info between server and client
+let port = 8080;            // exchange info between server and client
 
 app.listen(port, () => {            //listen is used to start a server and incoming requests
     console.log(`Server is running on port ${port}`);
 });
+
+
+// app.use((req, res) => {      // middleware function to handle requests
+//     console.log('Request received');
+//     res.send('Hello from Express server');   // send response to client
+// });
+
+//Learning about routing in express
+//with the help of routing we can handle different urls
+
+app.get('/',  (req , res) =>{                          // handling GET request to root URL
+    console.log('Request received at root path');
+    res.send('Hello, You contected root path');
+});
+
+app.get('/search',  (req , res) =>{ 
+    console.log('Request received at search path');
+    res.send('Hello, You contected search path');
+});
+
+app.get('/help',  (req , res) =>{
+    console.log('Request received at help path');
+    res.send('Hello, You contected help path');
+});
+
+//handling 404 error page
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
