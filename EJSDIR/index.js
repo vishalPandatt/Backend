2c87cc1f22684c1fa2d62452021b88e1336e8668
Vishal Vashishth <vishal112vp@gmail.com>
2025-11-20 12:17:05 +0530

    EJS

--- EJSDIR/index.js
+++ EJSDIR/index.js
@@ -1,16 +1,21 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');           // to work with file and directory paths, path use when we run our server from different location    
+
 
 const app = express();
 
 const port = 3000;
 
+app.set('views', path.join(__dirname, "views"));        // setting the views directory
 
 app.set('view engine', 'ejs');      // setting ejs as the view engine
 
 app.get('/',(req, res)=>{
-    res.render('home.ejs');              // rendering home.ejs file
+    res.render('home');              // rendering home.ejs file which is present in views folder
 })
 
 app.listen(port, () =>{
     console.log(`Server is running on ${port}`);
 }); 
+
+
