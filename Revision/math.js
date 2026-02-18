7d394bd4a43a543be2ede7f5a308790207ec838e
Vishal Vashishth <vishal112vp@gmail.com>
2026-02-18 10:24:03 +0530

    node

---
+++ Revision/math.js
@@ -0,0 +1,40 @@
+// If the file is using CommonJS module system, you can export functions like this:
+// 1st way to export the functions-
+
+// const add = (a, b) => a + b;
+// const sub = (a, b) => a - b;
+
+// module.exports = { add, sub };
+
+
+// 2nd way to export the functions-
+
+// exports.add = (a, b) => a + b;
+// exports.sub = (a, b) => a - b;
+
+
+//3rd way to export the functions-
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// module.exports.add = add;
+
+
+// If the file is using ES6 module system, you can export functions like this:
+
+// 1st way to export the functions-
+
+// export const add = (a, b) => a + b;
+// export const sub = (a, b) => a - b;
+
+
+// 2nd way to export the functions-
+
+// export default function add(a, b) {
+//   return a + b;
+// }
+
+
+
---
+++ Revision/script.js
@@ -0,0 +1,32 @@
+
+// we can require another file when module system is commonjs--
+
+// 1st way to import the file- 
+
+// const math = require('./math');
+
+// console.log(math.add(5, 2));
+// console.log(math.sub(5, 2));
+
+// 2nd way to import the file-
+
+// const { add, sub } = require('./math');
+
+// console.log(add(5, 2));
+// console.log(sub(5, 2));
+
+
+// If the file is using ES6 module system, you can import functions like this:
+
+// 1st way to import the file-
+
+// import { add, sub } from './math.js';
+// console.log(add(5, 2));
+
+// 2nd way to import the file-
+
+// import add from './math.js';
+// console.log(add(5, 2));
+
+
+
