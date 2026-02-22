31653fc45571c32b4221eee3a5456acd470eb28d
Vishal Vashishth <vishal112vp@gmail.com>
2026-02-22 18:56:08 +0530

    22-feb

--- Server/project 1/index.js
+++ Server/project 1/index.js
@@ -14,7 +14,41 @@ app.get("/users", (req, res)=>{
     ${users.map(user => `<li>${user.first_name}</li>`).join("")}
     </ul>`
     res.send(HTML);
+});
+
+
+// app.get("/api/users/:id", (req, res)=>{
+//     const id = Number(req.params.id);
+//     const user = users.find(user => user.id === id);
+//     return res.json(user);
+// });
+
+// app.patch("/api/user:id", (req, res)=>{
+//     return res.json({Status: "panding"});
+// });
+
+// app.delete("/api/user:id", (req, res)=>{
+//     return res.json({Status: "panding"});
+// });
+
+// we use /api/users/:id in three requests insted of three requests we can create a route and use three requests at once
+
+app.route("/api/users/:id")
+.get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user);
 })
+.patch((req, res) => {
+    return res.json({ status: "pending" });
+})
+.delete((req, res) => {
+    return res.json({ status: "pending" });
+});
+
+app.post("/api/user", (req, res) => {
+    return res.json({ status: "pending" });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
