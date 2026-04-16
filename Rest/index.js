73cbf291aba8171ab7074098925891d8b4963db7
Vishal Vashishth <vishal112vp@gmail.com>
2026-04-16 21:15:13 +0530

    making user UI

---
+++ Rest/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username: "vishal",
+        content: "This is my first post"
+    },
+    {
+        username: "Sharma Ji",
+        content: "This is my class"
+    },
+    {
+        username: "Pandit Ji",
+        content: "This is my code"
+    }
+];
+
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/posts', (req, res) => {
+    res.render('index', { posts: posts });
+});
