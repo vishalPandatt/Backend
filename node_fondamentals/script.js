241460d811ae8ce9c8137f305da517977fbc9a5d
Vishal Vashishth <vishal112vp@gmail.com>
2026-06-21 16:07:40 +0530

    ejs

--- node_fondamentals/script.js
+++ node_fondamentals/script.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.set("view engine", "ejs");
+
+app.use(function(req, res, next){
+    console.log("midddleware is running");
+    next(); 
 });
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/profile', (req,res) =>{
+    res.send("This is the profile page");
+});
+app.get('/profile/:username', (req,res) =>{
+    res.send(`Welcome to the profile page username: ${req.params.username}`);
+});
+
 
 app.listen(8000, ()=>{
     console.log("server is running on port 8000")
