fae062f897b5f1fde9148f1639981c1e911e6429
Vishal Vashishth <vishal112vp@gmail.com>
2026-06-23 23:17:30 +0530

    mongoose setup

--- node_fondamentals/script.js
+++ node_fondamentals/script.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static("./public"))
+app.use(express.static("./public"));
 
 app.use(function(req, res, next){
     console.log("midddleware is running");
