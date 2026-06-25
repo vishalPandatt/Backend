c60653f7d5e62d6cbacb9190a9ae6cbf54208d1e
Vishal Vashishth <vishal112vp@gmail.com>
2026-06-25 19:36:36 +0530

    new task attached

---
+++ node_fondamentals/task-1/script.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res){
+    res.render("index");
+});
+
+app.listen(8000);
