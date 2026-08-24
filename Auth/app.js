06f0dbc8a3cc99fa0d19f2cfcf9a15888269a881
Vishal Vashishth <vishal112vp@gmail.com>
2026-08-24 23:44:03 +0530

    setup tailwind and create index ejs file

--- Auth/app.js
+++ Auth/app.js
@@ -1,28 +1,18 @@
 const express = require('express');
 const app = express();
 const bcrypt = require("bcrypt");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-// app.use(cookieParser());
+app.set("view engine", "ejs");
 
-// app.get('/', (req, res) => {
-//     res.cookie("name","Vishal");
-//     res.send('Cookie set!');
-//     console.log(req.cookies);
-// });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser()); 
 
-// app.get('/getcookie', (req, res) => {
-//     console.log(req.cookies);
-// });
-
-
-// app.get('/', (req, res) => {
-//     console.log(req.cookies);
-//     bcrypt.genSalt(10, function(err, salt) {
-//     bcrypt.hash("mypassword", salt, function(err, hash) {
-//         console.log(hash);
-//     });
-// });
-// });
-
+app.get("/",(req, res) => {
+    res.render("index");
+});
 
 app.listen(3000);
