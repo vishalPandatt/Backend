b41162042cca77ceb72e46bd29d7843afcacfc47
Vishal Vashishth <vishal112vp@gmail.com>
2026-08-25 00:41:28 +0530

    setup password in hash

--- Auth/app.js
+++ Auth/app.js
@@ -1,18 +1,49 @@
 const express = require('express');
 const app = express();
+
 const bcrypt = require("bcrypt");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const userModel = require("./models/users");
+
 app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cookieParser()); 
+app.use(cookieParser());
 
-app.get("/",(req, res) => {
+app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.listen(3000);
+app.post("/create", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Generate salt
+        const salt = await bcrypt.genSalt(10);
+
+        // Hash password
+        const hash = await bcrypt.hash(password, salt);
+
+        console.log(hash);
+
+        // Create user with hashed password
+        const createdUser = await userModel.create({
+            email,
+            password: hash
+        });
+
+        res.send(createdUser);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error creating user");
+    }
+    console.log(password);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
--- Auth/models/users.js
+++ Auth/models/users.js
@@ -4,7 +4,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/auth");
 
 
 const userSchema = mongoose.Schema({
-    name:{type: String, required:true},
     email:{type: String, required:true},
     password:{type: String, required:true}
 });
